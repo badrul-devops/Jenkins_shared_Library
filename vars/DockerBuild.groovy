48935c721d18a27e55dc82259a59678b73381a2b
Badrul_alam <badrulalam0123@gmail.com>
2023-04-06 19:24:50 +0600

    add code

--- vars/DockerBuild.groovy
+++ vars/DockerBuild.groovy
@@ -6,3 +6,12 @@ def call(String project,String hubuser,String ImageTag){
 
     """
 }
+
+
+def call(String aws_account_id, String region, String ecr_repoName){
+    
+    sh """
+     docker build -t ${ecr_repoName} .
+     docker tag ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+    """
+}
